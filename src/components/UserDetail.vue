c85c2063793b2c39d124eaf0efc43e17a83cb38a
Miguel Peralta <eudysperalta723@gmail.com>
2020-06-06 22:01:01 -0700

    components and props

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+        <p>User age: {{ userAge }}</p>
+        <button @click="resetFn()"> Reset Name </button>
+        <button @click="resetName"> Reset Name </button>
+    </div>
+</template>
+
+<script>
+    import { eventBus } from "../main.js"
+
+    export default {
+        //prop validation
+        props: {
+            name: {
+                type: String
+            },
+            resetFn: Function,
+            userAge: Number
+        },
+        methods: {
+            resetName(){
+                this.name = "Miguel"
+                this.$emit('nameWasReset', this.name)
+            }
+        },
+        created(){
+            eventBus.$on("ageWasEdited", (age) => {
+                this.userAge = age
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
